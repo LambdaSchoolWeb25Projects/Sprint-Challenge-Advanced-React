af667963ca837d41aea89e766661696895e21434
DeniseFafette <denisefafette@gmail.com>
2019-12-06 10:29:18 -0800

    built App class component and imported Card component into App component to displayer player name and country

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
+//Create a class component to fetch data https://googletrends.github.io/data/
+// http://localhost:5000/api/players
+//Display player data
+//Build a custom hook (using localStorage or darkMode)
+//Write 3-5 tests that sufficiently test the user experience
+
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Card from './components/Card';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount(){
+    console.log('cDM is running')
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => this.setState({
+        players: res.data
+      }))
+      .catch(err => console.log('players error', err))
+  }
+
+  render() {
+    console.log('rendering App component');
+    return (
+      <div className="App">
+        <Card player={this.state.players} name={this.state.name} country={this.state.country} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Card(props) {
+    console.log('rendering Card component', props);
+    return (
+        <div>
+            {props.players.map(players => (
+                <div key={players.id}>
+                        <h2>{players.name}</h2>
+                        <h4>{players.country}</h4>
+                </div>
+            ))}
+        </div>
+    )
+}
